62bf46efa6925decaf7f28500ceb8400cb95ba8f
Tushar Mishra <67139908+TusharMishra9695@users.noreply.github.com>
2021-09-15 17:28:54 +0530

    Code Updated with Time validation and next page logic

--- components/Scheduler.js
+++ components/Scheduler.js
@@ -18,9 +18,11 @@ import weekSlotsData from "../utils/SchedulerData";
 export default function Scheduler(prop) {
   const [weekSlots, setWeekSlots] = useState(weekSlotsData());
   const [text, setText] = useState(true); 
+  const [texthandle, setTexthandle] = useState(true); 
   const [count,setCount] = useState(0);
-
   const [count2,setCount2] = useState(0);
+  
+
 
   // const updateValue = (key, data, day) => {                                                                                    
   //   const slots = { ...weekSlots };
@@ -47,37 +49,70 @@ export default function Scheduler(prop) {
             setWeekSlots(slots);
     console.log(currentSlot)
     console.log(day);
+   
+
+    // Here function accept the value which are passed in logic of next page and accordingly accept update
+    // the state of count for next page logic which are applied in next page button
     if(key === "isEnabled" && data === true ){
       setCount(count+1)
     }
     else if(key === "isEnabled" && data === false ){
       setCount(count-1)
+      if(text === true){
+      let from = null
+      updateValue("from",from,day)
+      let to = null
+      updateValue("to",to,day)
+      }
     }
-     else if(count ===7 && ((key === "from" && data !=null  ) ||  (key === "to" && data !=null  )) ){
+     else if( ((key === "from" && data !==null  ) ||  (key === "to" && data !=null  )) ){
+       
       setCount2(count2+1)
+       
+    }     else if( ((key === "from" && data ===null  ) ||  (key === "to" && data ===null  )) ){
+       
+      setCount2(count2-1)
+       
     }
-    else if(count !=7 && ((key === "from" && data ===null  ) ||  (key === "to" && data ===null  )))
-    setCount2(count2-1)
+
   };
-          const [date, setDate] = useState(new Date(1598051730000));
-          const [mode, setMode] = useState("date");
-          const [show, setShow] = useState(false);
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
           const onChange = (event, selectedDate) => {
             const currentDate = selectedDate || date;
             setShow(Platform.OS === "ios");
             setDate(currentDate);
             let tempDate = new Date(currentDate);
-             let fTime= tempDate.getHours() + ":" + tempDate.getMinutes();
-            if(text === true)
+             let fTime= tempDate.getHours() + ":" + tempDate.getMinutes() ;
+                let startTime,endTime,startTime2,endTime2;      
+                // here there is a boolean state which specify the slotType i.e
+                // if  boolean state(text) is true then slotType  is from else   slotType is to . 
+              if(text === true)
                {
-                let from = fTime 
-                updateValue("from",from,day)
+                let from = fTime
+                 startTime = moment(from,"HH:mm")
+                 startTime2 = moment(from, "hh:mm")
+
+                updateValue("from",from,day)                
                 setText(false)
               // alert(text)
             }
+            // here we are applying our logic for validation of time and for next page if time entered correctly 
+            //  then it will pass the value in function for  state  being update for next page
             else{
+              
               let to = fTime 
-              updateValue("to",to,day)
+               endTime = moment(to,"HH:mm" )
+               endTime2 = moment(to,"hh:mm" )
+              {
+                moment( endTime2).isAfter(startTime2) ||  moment( startTime2).isBefore(endTime2)  ?
+                updateValue("to",to,day)     
+                : (  alert( "Please fill time correctly"),
+                        setCount2(count2-1))
+          
+               }
+              
               setText(true)
               // alert(text)
 
@@ -91,9 +126,10 @@ export default function Scheduler(prop) {
           const showTimepicker = () => {
             showMode("time");
           };
-
-          let m = moment(from, "HH:mm");
-          let m2 = moment(to, "HH:mm");
+          // alert(data)
+// {
+//   data != true ?  updateValue("from",from,day)  : null
+// }
 
           return (
             <>
@@ -105,7 +141,7 @@ export default function Scheduler(prop) {
                 <View style={styles.handleView}>
                   <Text
                     style={styles.labelStyle}
-                    // onPress={click}
+                    
                   >
                     {day}
                   </Text>
@@ -114,7 +150,7 @@ export default function Scheduler(prop) {
                       onPress={showTimepicker}
                       style={styles.button}
                     >
-                      <Text>{from} </Text>
+                      <Text>{from}{from ? 'AM' :null}</Text>
                     </TouchableOpacity>
                   ) : (
                     <TouchableOpacity
@@ -122,27 +158,17 @@ export default function Scheduler(prop) {
                       style={styles.button}
                       disabled
                     >
-                      <Text>{from} </Text>
+                      <Text>{from}{from ? 'AM' :null}</Text>
                     </TouchableOpacity>
                   )}
-                  {moment(m2).isAfter(from) ? (
-                    <Text
-                      style={{
-                        fontSize: 16,
-                        color: "white",
-                        backgroundColor: "black",
-                      }}
-                    >
-                      Please Fill in correct format
-                    </Text>
-                  ) : null}
+               
                   <Text style={styles.labelStyle}>to</Text>
                   {isEnabled ? (
                     <TouchableOpacity
                       onPress={showTimepicker}
                       style={styles.button}
                     >
-                      <Text>{to}</Text>
+                      <Text>{to} {to ? 'PM' :null}</Text>
                     </TouchableOpacity>
                   ) : (
                     <TouchableOpacity
@@ -150,20 +176,9 @@ export default function Scheduler(prop) {
                       style={styles.button}
                       disabled
                     >
-                      <Text>{to}</Text>
+                      <Text>{to} {to ? 'PM' :null}</Text>
                     </TouchableOpacity>
                   )}
-                  {moment(m).isBefore(to) ? (
-                    <Text
-                      style={{
-                        fontSize: 16,
-                        color: "white",
-                        backgroundColor: "black",
-                      }}
-                    >
-                      Please Fill in correct format
-                    </Text>
-                  ) : null}
                   <Switch
                     trackColor={{ false: "black", true: "red" }}
                     thumbColor={isEnabled ? "white" : "white"}
@@ -208,7 +223,12 @@ export default function Scheduler(prop) {
       >
         <Text style={styles.handleText}>SCHEDULE</Text>
         <SchedulerTime />
-        {(count === 7 && count2 === 14) ?
+        <Text>{count2}</Text>
+        {/* here we select the two state i.e count and count2 and the intention is if all the buttons are closed and also count is equal to zero then go for next page 
+        or if count is multiple of 2 which is equal to count 2 the go for next page and multiple of 2 is applied because there are seven switch and
+         forteen input field for date time picker  so everything is in multiple of two .
+           */}
+        { ((count*2)   === (count2) ) || count ===0  ?
         <TouchableOpacity style={styles.nextButton}>
             <Text
               style={styles.textStyle}
@@ -216,7 +236,11 @@ export default function Scheduler(prop) {
             >
               Next
             </Text>
-          </TouchableOpacity> :       <TouchableOpacity style={styles.nextButton} disabled>
+          </TouchableOpacity> :       <TouchableOpacity style={styles.nextButton} 
+          onPress={() => alert("Please select time for all open slots ")
+        
+        }
+          >
             <Text
               style={styles.textStyle}
             >
